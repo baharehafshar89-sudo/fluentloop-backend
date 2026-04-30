98d37d064c545610d6e3a86dcccf6e6585bd4242
baharehafshar89-sudo <bahareh.afshar89@gmail.com>
2026-05-01 00:44:40 +0330

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
 
+const chat = document.getElementById("chat");
+
+async function send(){
+
+const input = document.getElementById("input");
+const text = input.value;
+
+if(!text) return;
+
+addMessage("user", text);
+input.value = "";
+
+const res = await fetch("https://fluentloop-backend.onrender.com/chat",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({message:text})
+});
+
+const data = await res.json();
+
+addMessage("ai", data.reply);
+
+}
+
+function addMessage(role,text){
+
+const div = document.createElement("div");
+div.className = "message " + role;
+div.innerText = text;
+
+chat.appendChild(div);
+chat.scrollTop = chat.scrollHeight;
+
+}
