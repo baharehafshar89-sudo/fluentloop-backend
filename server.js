ff1db5df1784378dff2fe531182aea1ff13c23b2
baharehafshar89-sudo <bahareh.afshar89@gmail.com>
2026-04-30 17:44:04 +0330

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,14 @@ app.post("/chat", async (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
